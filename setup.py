3a1aeafd13d8b004538e2659ff40443e629eb76c
Sohom Banerjee <sohom.ban966@gmail.com>
2022-04-20 19:20:24 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It is a wine quality package",
+    author="Sohom",
+    packages=find_packages(),
+    license="MIT"
+   )
